bcf590163b963e7ad976c23843d7d8fbdd4dc357
michael-chen-ics4u1-01-25 <michael.chen29@student.tdsb.on.ca>
2025-10-17 09:39:08 -0400

    completed set

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,5 +4,15 @@ public class PracticeProblem {
 
 	}
 
-
+	public static void insertionSort(char[] characters) {
+		for (int i = 1; i < characters.length; i++) {
+			char key = characters[i];
+			int index = 1;
+			while (i - index >= 0 && characters[i - index] > key) {
+				characters[i - index + 1] = characters[i - index];
+				index++;
+			}
+			characters[i - index + 1] = key;
+		}
+	}
 }
